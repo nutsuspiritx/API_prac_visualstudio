3f6d3556a63ef6cb93f4e17b7bdeb992eb010d1d
Thanat Leepaisarn <nutsuthanut@gmail.com>
2019-07-10 17:01:54 +0700

    Connect CRM

---
+++ API_prac_01/Calling Code.cs
@@ -0,0 +1,11 @@
+﻿ConnectionData connectionData = new ConnectionData()
+{
+    User = "Preuksa\\admin.crm",
+    Password = "Pruks@2016",
+    ServerAddress = "crmqa.pruksa.com",
+    OrganizationName = "CRMQA",
+    IsO365Org = false,
+    SSL = true
+};
+
+var service = ConnectionThreadSafe.GetOrganizationProxy(connectionData);
---
+++ API_prac_01/Class2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+        ConnectionData connectionData = new ConnectionData()
+        {
+            User = "Preuksa\\admin.crm",
+            Password = "Pruks@2016",
+            ServerAddress = "crmqa.pruksa.com",
+            OrganizationName = "CRMQA",
+            IsO365Org = false,
+            SSL = true
+        };
+
+        var service = ConnectionThreadSafe.GetOrganizationProxy(connectionData);
+    }
+}
